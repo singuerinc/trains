7732fb4de12c2f6d89f179f95adc2dc45468b471
singuerinc <nahuel.scotti@gmail.com>
2018-06-08 08:17:51 +0200

    feat: draw connections once

--- src/main.ts
+++ src/main.ts
@@ -1,68 +1,42 @@
-import paper from "paper";
-import * as R from "ramda";
-import { Node } from "./node";
+import paper from 'paper';
+import * as R from 'ramda';
+import { net } from './net';
 
-interface Line extends Array<Node> {}
-
-const mapIndexed = R.addIndex(R.map);
-
-const connect = (node1, node2) => {
-  node1.siblings = R.append(node2, node1.siblings);
-  node2.siblings = R.append(node1, node2.siblings);
+const connection = (n1, n2) => `${n1.id}+${n2.id}`;
+const connectionExist = (n1, n2, list) => {
+  return (
+    R.indexOf(connection(n1, n2), list) !== -1 ||
+    R.indexOf(connection(n2, n1), list) !== -1
+  );
 };
 
-const nums = [R.range(0, 5), R.range(0, 8), R.range(0, 4), R.range(0, 10)];
-
-const net: Line[] = mapIndexed((line, lIdx) => {
-  const nodes: Node[] = mapIndexed(
-    (n, idx) => new Node(`t-l1-${idx}`, 50 + 50 * lIdx, 50 + 20 * idx),
-    line as Line
-  ) as Node[];
-
-  const withSiblings = mapIndexed((node, idx, arr) => {
-    const sibling: Node = arr[idx - 1] as Node;
-    if (sibling) {
-      connect(
-        node,
-        sibling
-      );
-    }
-    return node;
-  }, nodes);
-
-  return withSiblings;
-}, nums) as Line[];
-
-connect(
-  net[0][3],
-  net[1][2]
-);
-
-connect(
-  net[1][0],
-  net[2][2]
-);
-
-connect(
-  net[2][3],
-  net[3][3]
-);
-
 window.onload = function() {
-  const canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
+  const canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
   paper.setup(canvas);
+
+  let connections = [];
 
   R.forEach(line => {
     R.forEach(node => {
       const circle = new paper.Path.Circle(new paper.Point(node.x, node.y), 3);
-      circle.strokeColor = "black";
+      circle.strokeColor = 'black';
       // path.lineTo(start.add([node.x, node.y]));
       R.forEach(sibling => {
-        const path = new paper.Path();
-        path.strokeColor = "black";
-        const start = new paper.Point(node.x, node.y);
-        path.moveTo(start);
-        path.lineTo(new paper.Point(sibling.x, sibling.y));
+        if (!connectionExist(node, sibling, connections)) {
+          console.log('NO ', connection(node, sibling));
+          const path = new paper.Path();
+          path.strokeColor = new paper.Color(
+            Math.random(),
+            Math.random(),
+            Math.random()
+          );
+          const start = new paper.Point(node.x, node.y);
+          path.moveTo(start);
+          path.lineTo(new paper.Point(sibling.x, sibling.y));
+          connections = R.append(connection(node, sibling), connections);
+        } else {
+          console.log('YES', connection(node, sibling));
+        }
       }, node.siblings);
     }, line);
   }, net);
---
+++ src/net.ts
@@ -0,0 +1,51 @@
+import * as R from 'ramda';
+import { Node } from './node';
+
+interface Line extends Array<Node> {}
+
+const mapIndexed = R.addIndex(R.map);
+
+const connect = (node1, node2) => {
+  node1.siblings = R.append(node2, node1.siblings);
+  node2.siblings = R.append(node1, node2.siblings);
+};
+
+const net: Line[] = mapIndexed(
+  (line, lIdx) => {
+    const nodes: Node[] = mapIndexed(
+      (n, idx) => new Node(`t-l${lIdx}-${idx}`, 50 + 50 * lIdx, 50 + 20 * idx),
+      line as Line
+    ) as Node[];
+
+    const withSiblings = mapIndexed((node, idx, arr) => {
+      const sibling: Node = arr[idx - 1] as Node;
+      if (sibling) {
+        connect(
+          node,
+          sibling
+        );
+      }
+      return node;
+    }, nodes);
+
+    return withSiblings;
+  },
+  [R.range(0, 5), R.range(0, 8), R.range(0, 4), R.range(0, 10)]
+) as Line[];
+
+connect(
+  net[0][3],
+  net[1][2]
+);
+
+connect(
+  net[1][0],
+  net[2][2]
+);
+
+connect(
+  net[2][3],
+  net[3][3]
+);
+
+export { net };
